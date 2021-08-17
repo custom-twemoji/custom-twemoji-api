cb6f09603871443efea4dea9c4b0e41d57aac509
Blake Gearin <blakegearin@pm.me>
2021-08-16 23:25:12 -0500

    Reorganizing and simplifying code

--- app.rb
+++ app.rb
@@ -14,41 +14,24 @@ DEFAULT_STACKING_ORDER = [
 
 get '/' do
   params = validate_and_symbolize
-
-  def get_file(id)
-    File.open("assets/#{id}.svg") { |f| Nokogiri::XML(f) }
-  end
-
-  def get_part_from_file(part, file_name)
-    file = get_file(file_name)
-    file.at_css("[id='#{part}']")
-  end
-
-  def write_output(file)
-    xml = file.to_xml
-
-    File.open("tmp/out.svg", "w") do |f|
-      f.write(xml)
-    end
-
-    xml
-  end
+  return if params.empty?
 
   base_file = get_file('base').at(:svg)
 
   if params[:order] == 'manual'
-    params.each do |key, value|
-      next if key == :order
-      base_file.add_child(get_part_from_file(key, value).to_s) unless value.nil?
+    params.except(:order).each do |key, value|
+      next if value.nil?
+      base_file.add_child(get_part_from_file(key, value).to_s)
     end
   else
     DEFAULT_STACKING_ORDER.each do |key|
       value = params[key]
-      base_file.add_child(get_part_from_file(key, value).to_s) unless value.nil?
+      next if value.nil?
+      base_file.add_child(get_part_from_file(key, value).to_s)
     end
   end
 
-  write_output(base_file)
+  output_svg(base_file)
 end
 
 private
@@ -57,7 +40,26 @@ def validate_and_symbolize
   valid_params = [DEFAULT_STACKING_ORDER, :order].flatten
   Hash[
     params.map do |(k,v)|
-      [k.to_sym,v]
+      [ k.to_sym, v ]
     end
   ].select { |key, value| valid_params.include?(key) }
+end
+
+def get_file(id)
+  File.open("assets/#{id}.svg") { |f| Nokogiri::XML(f) }
+end
+
+def get_part_from_file(part, file_name)
+  file = get_file(file_name)
+  file.at_css("[id='#{part}']")
+end
+
+def output_svg(file)
+  xml = file.to_xml
+
+  File.open("tmp/out.svg", "w") do |f|
+    f.write(xml)
+  end
+
+  xml
 end
